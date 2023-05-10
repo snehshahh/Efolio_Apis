e7b59ab1f07fa1ea91a25ad9e2944f5ddb1bd827
Sneh Shah <sneh.shah.sapphire@gmail.com>
2023-05-10 17:02:01 +0530

    updated code Registration Of the user

--- Efolio_Api/Controllers/LoginController.cs
+++ Efolio_Api/Controllers/LoginController.cs
@@ -20,11 +20,11 @@ namespace Efolio_Api.Controllers
         }
 
         // GET: api/<LoginController>
-        [HttpGet]
+        [HttpGet("IsUserCredentialsValid")]
         public IActionResult IsUserCredentialsValid([FromQuery] string email, [FromQuery] string password)
         {
             string isValidCredentials = dbHelper.IsUserCredentialsValid(email, password);
-            if (isValidCredentials != " ")
+            if (isValidCredentials != " " && isValidCredentials != null)
             {
                 // User credentials are valid, perform further actions like generating a token or returning a success response
                 return Ok("Login successful");
@@ -33,5 +33,22 @@ namespace Efolio_Api.Controllers
             // User credentials are invalid, return an appropriate response
             return Unauthorized("Invalid email or password");
         }
-    }
+
+        [HttpGet("RegisterAndAuthenticateUser")]
+		public IActionResult RegisterAndAuthenticateUser([FromQuery] string email, [FromQuery] string password)
+		{
+			// Create a new user entity with the provided email and password
+			var login = new Login
+			{
+				Email = email,
+				Password = password
+			};
+            string url = dbHelper.RegisterAndAuthenticateUser(login);
+            return Ok(url);
+
+		}
+
+
+
+	}
 }
--- Efolio_Api/Models/DbHelper.cs
+++ Efolio_Api/Models/DbHelper.cs
@@ -8,26 +8,36 @@ namespace Efolio_Api.Models
     {
         private EF_DataContext _context;
         public DbHelper(EF_DataContext context) {  _context = context; }
-        public string IsUserCredentialsValid(string email, string password)
-        {
-            // Check if there is a user with the given email and password
-            bool isValidUser = _context.Logins.Any(u => u.Email == email && u.Password == password);
+		public string IsUserCredentialsValid(string email, string password)
+		{
+			// Check if there is a user with the given email and password
+			bool isValidUser = _context.Logins.Any(u => u.Email == email && u.Password == password);
 
+			if (isValidUser)
+			{
+				var link = _context.Links.FirstOrDefault(l => l.Email == email);
 
-            if (isValidUser)
-            {
-                var link = _context.Links.FirstOrDefault(l => l.Email == email);
+				if (link != null)
+				{
+					return link.GLink;
+				}
+			}
 
-                if (link != null)
-                {
-                    return link.GLink;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            //return isValidUser;
-        }
-    }
+			return null;
+		}public string RegisterAndAuthenticateUser(Login login)
+		{
+			_context.Logins.Add(login);
+			_context.SaveChanges();
+			string url = $"http://example.com?email={login.Email}";
+			var links = new Link()
+			{
+				Email = login.Email,
+				GLink = url
+			};
+			_context.Links.Add(links);
+			return url;
+
+		}
+
+	}
 }
